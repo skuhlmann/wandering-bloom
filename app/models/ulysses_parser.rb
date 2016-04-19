461df9ddf20889b703356c775889a4845792a6ed
skuhlmann <samkuhlmann@gmail.com>
2016-04-19 16:22:17 -0600

    Works on the chapter parser

--- app/models/ulysses_parser.rb
+++ app/models/ulysses_parser.rb
@@ -1,7 +1,7 @@
 require 'net/http'
 
 class UlyssesParser
-  attr_accessor :raw_text
+  attr_accessor :raw_text, :chapter, :file_path
 
   def initialize(chapter)
     @chapter_name = chapter[:name]
@@ -9,19 +9,39 @@ class UlyssesParser
     @domain = "faculty.georgetown.edu"
     @file_path = build_file_path
     @raw_text = fetch_raw_text
+    @chapter = create_chapter
   end
 
-  def parse_chapter
-
+  def create_chapter
+    chapter = Chapter.where(name: @chapter_name)
+    if chapter.empty?
+      new_chapter = { name: @chapter_name, number: @chapter_number }
+      chapter = Chapter.create(new_chapter)
+    end
+    chapter
   end
 
-  private
+  def parse_chapter_text
+    # if chapter 15 deal with <* page *> first
+    @raw_text = strip_text
+    split = @raw_text.split(/(\.|\?|!)/)
+    #need to break into sentences now
+    #probably need a mass insert fucker
+  end
+
+  def strip_text
+    @raw_text.gsub!(/\n/, " ").squeeze!(" ")
+    text = @raw_text.partition("*")[2].strip
+    text.split("*").join.squeeze(" ")
+  end
 
   def fetch_raw_text
     Net::HTTP.get(@domain, @file_path)
   end
 
   def build_file_path
-    "/jod/ulysses/ulys#{@chapter_number}.txt"
+    number = @chapter_number
+    number = "#{number}dr" if @chapter_number == 18
+    "/jod/ulysses/ulys#{number}.txt"
   end
 end
--- spec/models/ulysses_parser_spec.rb
+++ spec/models/ulysses_parser_spec.rb
@@ -9,13 +9,36 @@ RSpec.describe UlyssesParser, :type => :model do
     expect(raw_text.is_a?(String)).to eq(true)
   end
 
-  xit "can parse a chapter into sentences" do
-    parser = UlyssesParser.new(22)
+  it "builds the correct file path for last chapter" do
+    parser = UlyssesParser.new(chapter = { name: "Penelope", number: 18 })
+
+    expect(parser.file_path).to eq("/jod/ulysses/ulys18dr.txt")
+  end
+
+  it "creates a chapter" do
+    chapter = { name: "Telemachus", number: 1 }
+    parser = UlyssesParser.new(chapter)
+
+    expect(parser.chapter.name).to eq("Telemachus")
+  end
+
+  it "doesn't create duplicate chapters" do
+    chapter = { name: "Telemachus", number: 1 }
+    Chapter.create(chapter)
+    parser = UlyssesParser.new(chapter)
+
+    results = Chapter.where(name: "Telemachus")
+
+    expect(results.count).to eq(1)
+  end
+
+  it "can parse a chapter into sentences" do
+    parser = UlyssesParser.new(chapter = { name: "Telemachus", number: 22 })
     parser.raw_text = "
       8
-      * Pineapple rock, lemon platt, butter scotch. A sugarsticky girl
-      shovelling scoopfuls of creams for a christian brother. Some school treat."
-    parser.parse_chapter
+      * Pineapple rock. lemon platt, butter scotch? A sugarsticky girl
+      shovelling scoopfuls of creams for a christian brother! ** Some school treat."
+    parser.parse_chapter_text
 
     senteces = Sentence.all
     expect(enteces.count).to eq(3)
